5f78a611103c00b14b0c98a3b68113fe3f52887c
somonor011 <somonorhong011@gmail.com>
2025-02-07 20:12:08 +0700

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div>
+    <button @click="chageText()">Change Text</button>
+    <h1>{{ txt }}</h1>
+  </div>
+</template>
+
+<script>
+  
+  // Option API
+  export default{
+    data() {
+      return {
+        txt : "Hello VueJS",
+      }
+    },
+    methods: {
+      chageText(){
+        this.txt = "What's up VueJS ?"
+      }
+    },
+  }
+
+</script>
